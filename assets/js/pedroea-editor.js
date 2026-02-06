19a28ed22b83fd1ed27d538936b0a34960f1b3ee
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-02-06 18:55:24 +0600

    Add files via upload

---
+++ assets/js/pedroea-editor.js
@@ -0,0 +1,49 @@
+
+(function($) {
+    'use strict';
+    
+    $(window).on('elementor:init', function() {
+        
+        function addCustomCss(css, context) {
+            
+                if (!context || !context.model) {
+                    return css;
+                }
+                
+                let model = context.model;
+                let settings = model.get('settings');
+                
+                if (!settings) {
+                    return css;
+                }
+                
+                let customCss = settings.get('pedro_custom_css');
+                
+                if (!customCss || customCss.trim() === '') {
+                    return css;
+                }
+                
+                let elementId = model.get('id');
+                let selector = '.elementor-element.elementor-element-' + elementId;
+                
+                if ('document' === model.get('elType')) {
+                    selector = 'body';
+                }
+                
+                if (customCss) {
+                    css += customCss.replace(/selector/g, selector);
+                }
+                
+               return DOMPurify.sanitize( css, { CSS: true } );
+                
+        }
+        
+        if (typeof elementor !== 'undefined' && elementor.hooks) {
+            elementor.hooks.addFilter('editor/style/styleText', addCustomCss);
+        }
+    });
+    
+})(jQuery);
+
+
+
